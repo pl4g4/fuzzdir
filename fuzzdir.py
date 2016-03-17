b97075cf23d01c97013f4747561795204ced3053
R0n <pl4g4@users.noreply.github.com>
2016-03-17 11:45:48 -0500

    Update fuzzdir.py
    changed get to head for faster response, Only header is needed. Some small updates.

--- fuzzdir.py
+++ fuzzdir.py
@@ -10,15 +10,23 @@ parser.add_argument("--dic", help="The path where the txt file is located", type
 args = parser.parse_args()
 	
 def readFile(filePath):
+	CURSOR_UP_ONE = '\x1b[1A'
+	ERASE_LINE = '\x1b[2K'
 	if os.path.exists(filePath):
 		with open(filePath, 'r') as item:
 			try:
-				for line in item:
-					request = requests.get(args.url+'/'+line)
-					if request.status_code == 200:
-						print( Fore.GREEN + "Status Code: " + str(request.status_code) + ' ------------ Path: '+ line)
+				for line in item:											
+					response = requests.head(args.url+'/'+line)
+					if response.status_code == 200:
+						print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+						print( Fore.GREEN + "FOUND ---  Status Code: " + str(request.status_code) + ' --- Path: '+ args.url+'/'+line +'\n')
+					else:						
+						print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+						print("Checking  Path: "+ args.url+'/'+line ),				
 			except Exception as e:
 				print e
+
+			print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
 	else:
 		print "File not Found"
 	
@@ -27,11 +35,12 @@ def main(args):
 	if (len(sys.argv) < 2) | (len(sys.argv) > 4):
 		sys.exit('Invalid number of parameters, use -h for help')
 
-	print("=========================================================================")
-	print("                 Finding possible paths in "+args.url+ "                 ")
-	print("=========================================================================")
+	print("===================================================================================================")
+	print("                 	    Finding possible paths in "+args.url+ "                                   ")
+	print("===================================================================================================")
+	print('')
 	readFile(args.dic)
-	print("=========================================================================")
+	print("===================================================================================================")
 
 if __name__ == '__main__':
 	main(args)
